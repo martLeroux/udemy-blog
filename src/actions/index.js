60d2febcb93176a083f0dfe7acd10b40b35c9973
Martin Leroux <tintin_151@hotmail.com>
2018-05-26 14:27:58 -0400

    Ajoute la fonctionnalité pour supprimer un post

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
 export const FETCH_POST = 'fetch_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=mleroux';
@@ -26,9 +27,19 @@ export function createPost(values, callback) {
 
 export function fetchPost(id) {
 	const request = axios.get(`${ ROOT_URL }/posts/${id}/${ API_KEY }`);
-	console.log(request);
+
 	return {
 		type: FETCH_POST,
 		payload: request
+	}
+}
+
+export function deletePost(id, callback) {
+	const request = axios.delete(`${ ROOT_URL }/posts/${id}/${ API_KEY }`)
+		.then(() => callback());
+
+	return {
+		type: DELETE_POST,
+		payload: id
 	}
 }
